caab8ba1913f1fa7d7e8c7b4d4768bacee506644
JenniD5 <elizabethduran0105@gmail.com>
2019-08-29 12:38:51 -0700

    Agregar archivos de proyecto.

---
+++ nulos/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nulos
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public List<string>Materias { get; set; }
+
+        public Alumno ()//para uque cunado se cree una instancia nueva de alumno, se puede tener propiedades 
+        {
+            Materias = new List<string>();//inicializar la lista 
+        }
+        //los nulls sirev para liberar espacio, en vez de hacelro manualmente
+
+    }
+}
---
+++ nulos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nulos
+{
+    class Program
+    {
+        static Alumno alumno;
+
+        static void Main(string[] args)
+        {
+           alumno = new Alumno();//se el asigna un espacio en la memoria para que tenga los atributis y metodos de la clse aluumno
+
+
+           alumno.Materias.Add("matematicas");
+
+            ImprimirAlumno();
+            Console.Read();
+            
+        }
+
+        static void ImprimirAlumno()// para que o marque error, ya que es nula, y debes de inicializarla antes
+            //static para poderla usar. se usa cuando dentro de toda la aplicacion solo abra una como esa 
+
+        {
+            try//PAR AQUE NO SE PARE EL PROGRAMa
+            {
+                alumno.Nombre = "jose"; //debe de ser static Alumno,alumno para que no marque error 
+                Console.WriteLine("nombre " + alumno.Nombre);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);//viene el mensaje de la escepcion
+            }
+            
+            //Alumno alumno = new Alumno(); de forma manual en vez de hacerlo con el null
+            //alumno.Nombre = "jose"; //debe de ser static Alumno,alumno para que no marque error 
+            //Console.WriteLine("nombre "  + alumno.Nombre);
+            
+        }
+    }
+}
